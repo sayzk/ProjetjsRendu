8bed202a13d9f690e5a9b6f487686edc773c7e5b
sayzk <SAIDA.ZAKI@latoile.em-lyon.com>
2025-12-17 11:52:23 +0100

    ok

--- rendu/script.js
+++ rendu/script.js
@@ -47,25 +47,57 @@ fetch(API_URL)
 
          // Contenu HTML de la carte
          carte.innerHTML = `
-            <img src="["image-url"]
-            <h1>${produit.nom}</h1>
+            <img src="${produit["image-url"]}"
+            <h2>${produit.nom}</h2>
             <p>${produit.description}</p>`;
 
-            // tableau[nombre]
-            // objet.propriete
-            // dictionnaire["propriete"]
+         // tableau[nombre]
+         // objet.propriete
+         // dictionnaire["propriete"]
          // Ajouter la carte dans le container
          produitcontainer.appendChild(carte);
       });
-      // section services
-       const servicesContainer = document.getElementById("servicescontent");
-         console.log(data.services);
 
-         data.services.forEach(service => {
+
+      // section services
+      const servicesContainer = document.getElementById("servicescontent");
+      console.log(data.services);
+
+      data.services.forEach(service => {
          console.log(service);
 
-         const test = document.createElement("div");
-         test.textContent = `${service}`;
-         servicesContainer.appendChild(test);
+         // Créer une carte
+         const carte = document.createElement("div");
+         carte.classList.add("servicesCard");
+
+         carte.innerHTML = `
+            <h2>${service.nom}</h2>
+           <p>${service.description}</p>`;
+
+         console.log(servicesContainer);
+
+         servicescontent.appendChild(carte);
+      });
+
+
+      // section notes
+      const notescontainer = document.getElementById("notes")
+      console.log(data.temoignages); //Ceci est un tableau, donc on fait une boucle pour chaque element (loïc)
+
+      // pour chaque temoignage
+      data.temoignages.forEach(temoignages => {
+         console.log(temoignages);
+         // creer une carte
+         const carte = document.createElement("div");
+         carte.classList.add("notescard");
+         // ajouter innerhtml
+         carte.innerHTML = `
+            <h3>${temoignages.prenom}</h3>
+           <p>${temoignages.typeExperience}</p>
+           <p>${temoignages.commentaire}</p>`;
+         // mettre la carte dans le container
+         console.log(notescontainer);
+      notescontainer.appendChild(carte)
+
       });
    })
